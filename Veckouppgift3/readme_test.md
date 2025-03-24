3f077b3a031d89dbfe0dd470284b9f32b9a91385
Matzen(65) <matzeliasson@gmail.com>
2025-03-24 17:20:45 +0100

    veckouppgift3 färdig kod

--- Veckouppgift3/readme_test.md
+++ Veckouppgift3/readme_test.md
@@ -3,29 +3,28 @@ https://playwright.dev/docs/other-locators
 ### Test Timer-Vue
 
 ### User story [U1] - Verify "Add timer" button
-Som användare vill jag kunna öppna och stänga en widgets för en Timer 
-och ta bort densamma.
+Som användare vill jag kunna öppna och stänga ett fönster för widgeten timer
+och ta bort (Erase) densamma.
 
 ### Acceptanskriterier [A1]
-[A1.1] När jag klickar på knappen "Add timer", ska en timer skapas på skärmen.
-[A1.2] När jag klickar på icon "Add timer" ska timern dyka upp
-[A1.3] När jag klickar på icon för papperskorg ska Timern släckas
+[A1.1] Navigera till timer-vue app sida, verifiera att knappen "Add timer" syns 
+[A1.2] När jag klicka på icon "Add timer" ska timern dyka upp
+[A1.3] När jag klickar på icon för Erase/papperskorg ska Timer widgeten släckas
 
 ### Testscenario [T1]
 [T1.1] Navigera till webbsidan. Verifiera att endast en widget har öppnats
-[T1.2] Kontrollera att knappen "Add timer" finns och kan klickas,
+[T1.2] Kontrollera att knappen "Add timer" finns och kan klickas, verifiera att en ny widget öppnats
 [T1.3] Verifiera funktionen av knappen Erase och verifiera att nu är endast en widget öppen.
-
 
 # -----------------------------------------------
 
 ### User story [U2] - kunna interagera med timer
 Som användare vill jag kunna se att timern startar från 15:00 
-och att den kan starta pausas samt återställas. 
+och att den kan starta samt pausa. 
 
 ### Acceptanskriterier [A2]
 [A2.1] Timern ska starta från 15:00.
-[A2.2] Testa att Start kan klickas, testa att paus och erase kan klickas
+[A2.2] Testa att Start/paus kan klickas
 
 ### Testscenario [T2] – Timer-interaktioner
 [T2.2] kontrollera att timern startar med texten 15:00
@@ -33,13 +32,16 @@ och att den kan starta pausas samt återställas.
 
 #------------------------------------------------
 
-### User story [U3] - Ändra på tiden i timer-inställning
+### User story [U3] - Ändra på tiden i timer-inställning, Verifiera/återställ till 15:00,
 Som användare vill jag kunna ändra tiden i timern.
-Så att jag kan starta en bestämd nedräkning i timer.
+Så att jag kan starta en nedräkning med valfritt antal minuter i timer.
+
+### Acceptanskriterier [A3]
+[A3.1] Timern ska starta från 15:00.
+[A3.2] Testa att Start/paus kan klickas
 
 [T3.1] Ändra tiden i textfönstret från 15:00 till "5:00" samt verifiera texten 5:00. 
-kontrollera Reset till 15:00 samt avsluta med att stänga widgeten
-
+[T3.2] Reset till 5:00 samt avsluta med att stänga/erase widgeten
 
 # -----------------------------------------------
 
@@ -51,10 +53,7 @@ och kan klickas. Så att man är redo att göra en note.
 [A4.1] När jag klickar på knappen, ska en text "Click to change text" visas på skärmen.
 
 ### Testscenario [T4]
-[T4.1] Navigera till webbsidan.
-[T4.2] Kontrollera att knappen "Add note" finns och kan klickas
-
-
+[T4.1] Navigera till webbsidan. Kontrollera att knappen "Add note" finns och kan klickas
 
 # -----------------------------------------------
 
@@ -63,19 +62,18 @@ Som användare vill jag kunna se att knappen 'Add note' kan klickas och addera t
 Så att jag själv kan påbörja en note spara, ändra, byta ordning samt radera densamma.
 
 ### Acceptanskriterier [A5]
-[A5.1] Det ska vara möjligt att klicka på rutan med texten "Click to change text" och ändra innehållet.
-[A5.2] När man trycker "Enter", sparas texten.
-[A5.3] När man trycker på korgen, suddas texten bort.
-[A5.4] Genom att trycka på pilarna ska man kunna ändra ordning på sina notes.
+[A5.1] Det ska vara möjligt att finna och klicka på knappen Add note 
+[A5.2] Det ska vara möjligt att se och klicka på texten "Click to change text"
+[A5.3] Det ska gå att mata in text i fältet, klicka enter, samt se texten
+[A5.4] Det ska gå att öppna ett nytt fält samt mata in text i fältet, klicka enter, samt se texten
+[A5.5] ordningen på de två fälten ska kunna kastas om
+[A5.6] När man trycker på erase/delete, försvinner den widgeten.
 
 ### Testscenario [T5] – Kontrollera att knappen 'Add note', addera och delete note!
-[T5.1] Navigera till webbsidan.
-[T5.2] klicka på knappen "Add notes"
-[T5.3] Klicka/markera textrutan "Click to change text" 
-[T5.4] Interagera/markera input fältet och skriv texten "TEST T4.4"
-[T5.5] klicka med "Enter" för att spara
-[T5.6] Kontrollera att texten har ändrats till "Test T4.4"
-[T5.7] Enligt samma scenario från (T4.2 - T4.6) och fyll i en ny ruta med texten "TEST T4.7" samt kontrollera att det gjorts.
-[T5.8] Testa att ändra ordningen på de två texterna samt kontrollera att det gjorts
-[T5.9] Testa att radera med "close button", samt kontrollera att det gjorts
+[T5.1] Navigera till webbsidan. Klicka på knappen "Add notes" 
+[T5.2] Klicka/markera textrutan "Click to change text" 
+[T5.3] Interagera/markera input fältet och skriv texten "TEST T5.3", klicka med "Enter" för att spara Kontrollera att texten har ändrats till "Test T5.3"
+[T5.4] Enligt samma scenario från (T5.2 - T5.4) och fyll i en ny ruta med texten "TEST T5.4" samt kontrollera att det gjorts.
+[T5.5] Testa att ändra ordningen på de två texterna samt kontrollera att det gjorts
+[T5.6] Testa att radera "TEST T5.3" med "close button", samt kontrollera att det gjorts
 
--- Veckouppgift3/src/test_timer_app.py
+++ Veckouppgift3/src/test_timer_app.py
@@ -10,18 +10,20 @@ def test_verify_add_timer_remove_timer(page: Page):
     Kontrollera att knappen 'Add timer' är synlig och kan klickas och öppna timern,
     prova sedan att släcka timer-widgeten med close samt verifiera att den är stängd
     """
+
     # T1.1 - Navigera till timer app sidan, försäkra att inga andra aktiva widgets finns på sidan
     page.goto("https://lejonmanen.github.io/timer-vue/")
     timer_widget = page.locator('.widget')
-    expect(timer_widget).to_have_count(1, timeout=200) # endast en widget öppen
 
-    # T1.2 - Kontrollera att knappen "Add timer" finns och kan klickas samt verifiera erase
     add_timer_button = page.locator('button:has-text("Add timer")')
     expect(add_timer_button).to_be_visible()
+    expect(timer_widget).to_have_count(1, timeout=200) # endast en widget öppen
+
+    # T1.2 - Kontrollera att knappen "Add timer" kan klickas och verifiera att en ny widget skapats
     add_timer_button.click() # här öppnas en ny widget (nr2)
+    expect(timer_widget).to_have_count(2, timeout=200)  # widget nr 2 öppen och verifierat
 
     # T1.3 Klicka knappen Erase och verifiera att det därefter endast är en widget öppen.
-    expect(timer_widget).to_have_count(2, timeout=200)
     erase_button = timer_widget.locator('.icon.close') # stänger ner widget nr 2
     erase_button.click()
     expect(timer_widget).to_have_count(1, timeout=200) #verifierar att widget nr 2 har stängts
@@ -29,22 +31,21 @@ def test_verify_add_timer_remove_timer(page: Page):
 
 def test_interact_with_timer(page: Page):
     """
-    kunna interagera med widgeten timer, ställa tid, starta, paus och Reset
+    kunna interagera med widgeten timer, start/paus och Reset
     """
 
     # T2.1 - kontrollera att widget för timer har starttid 15:00 samt verifiera start/paus/reset och erase.
     page.goto("https://lejonmanen.github.io/timer-vue/")
     timer_widget = page.locator('.widget')
-    expect(timer_widget).to_have_count(1, timeout=200)
 
+    expect(timer_widget).to_have_count(1, timeout=200)
     add_timer_button = page.locator('button:has-text("Add timer")')
     expect(add_timer_button).to_be_visible()
     add_timer_button.click()  # här öppnas en ny widget (nr2)
+    timer_time_first = timer_widget.locator('.row.time').get_by_text(re.compile("15:00", re.IGNORECASE))
+    expect(timer_time_first).to_be_visible(timeout=200)
 
-    timer_time_before = timer_widget.locator('.row.time').get_by_text(re.compile("15:00", re.IGNORECASE))
-    expect(timer_time_before).to_be_visible(timeout=200)
-
-    #T2.2 - kontrollera om Start/Paus/Reset är synliga och klickbara
+    #T2.2 - kontrollera om Start/Paus är synliga och klickbara
     start_button = page.locator('button:has-text("Start")')
     expect(start_button).to_be_visible()
     start_button.click()
@@ -53,43 +54,51 @@ def test_interact_with_timer(page: Page):
     expect(pause_button).to_be_visible()
     pause_button.click()
 
-    reset_button = page.locator('button:has-text("Reset")')
-    expect(reset_button).to_be_visible()
-    reset_button.click()
-
 
 def test_change_time_for_timer(page: Page):
     """
-    # T3.1 Ändra tiden i textfönstret från 15:00 till "5:00" samt verifiera 5:00.
-    # kontrollera Reset till 15:00 samt avsluta med att nollställa
+    # T3 Ändra tiden i textfönstret från 15:00 till "5:00" samt verifiera 5:00.
+    # kontrollera att "Reset" återställer till 15:00 samt avsluta med att stänga
+    wwidgeten via iconen Close/papperskorgen samt verifiera stängt widgetfönster
     """
+    # T3.1 - Ändra tiden i textfönstret från 15:00 till "5:00" samt verifiera 5:00
+    page.goto("https://lejonmanen.github.io/timer-vue/")
     timer_widget = page.locator('.widget')
-    add_timer_button = page.locator('button:has-text("Add timer")')
-    expect(add_timer_button).to_be_visible()
-    add_timer_button.click()  # här öppnas en ny widget (nr2)
 
-    timer_widget.locator(".icon.settings").click(timeout=200)
+    add_timer_button = page.locator('button:has-text("Add timer")')
+    add_timer_button.click()  # här öppnas en ny widget (nr2)
+    timer_time = timer_widget.locator('.row.time').get_by_text(re.compile("5:00", re.IGNORECASE))
+    expect(timer_time).to_be_visible(timeout=200)
+    timer_widget.locator(".icon.settings").click(timeout=200) # öppna edit fönstret
     timer_input = timer_widget.get_by_role("textbox")
-    timer_input.fill("5")
-    timer_time_after = timer_widget.locator(".row.time").get_by_text(re.compile("5:00", re.IGNORECASE))
-    expect(timer_time_after).to_be_visible(timeout=200)
+    timer_input.fill("5:00") # skriver in 5:00 sekunder
+    timer_time = timer_widget.locator(".row.time").get_by_text(re.compile("5:00", re.IGNORECASE))
+    expect(timer_time).to_be_visible(timeout=200)
+
+    # T3.2 - sätt timern tillbaka till 15:00 samt stäng widgeten
+
+    expect(timer_widget).to_have_count(2, timeout=200) # säkerställer att två widgets är öppna
+    reset_button = page.locator('button:has-text("Reset")')
+    expect(reset_button).to_be_visible()
+    #OBS reset återställer enbart till förinställt startvärde, nu är det fortfarande ställt på "5:00"
+    timer_time = timer_widget.locator(".row.time").get_by_text(re.compile("5:00", re.IGNORECASE))
+    expect(timer_time).to_be_visible(timeout=200)
+    reset_button.click(timeout=200)
 
     erase_button = timer_widget.locator('.icon.close')
     erase_button.click()
-    expect(timer_widget).not_to_be_visible()
     expect(timer_widget).to_have_count(1, timeout=200)  # verifierar att widget nr 2 har stängts
 
 ###########################################################
 
 def test_verify_add_note_button(page: Page):
     """
-    Testscenario [T3]
+    Testscenario [T4]
     Kontrollera att knappen "Add note" är synlig och kan klickas
     """
-    # T3.1 - Navigera till timer app sidan
+    # T4.1 - Navigera till timer app sidan och klicka på knappen "Add note"
     page.goto("https://lejonmanen.github.io/timer-vue/")
 
-    # T3.2 Kontrollera att knappen "Add note" finns och är synlig och kan klickas
     add_note_button = page.locator('button:has-text("Add note")')
     expect(add_note_button).to_be_visible()
     add_note_button.click()
@@ -97,55 +106,50 @@ def test_verify_add_note_button(page: Page):
 
 def test_interaction_add_note_button(page: Page):
     """
-    Testscenario [T4]
+    Testscenario [T5]
     Kontrollera att knappen "Add note" är synlig och kan interageras med
     """
-    # T4.1 - Navigera till timer app sidan
+    # T5.1 - Navigera till timer app sidan
     page.goto("https://lejonmanen.github.io/timer-vue/")
-
-    # T4.2 Klicka på knappen "Add note"
     #Playwright adds custom pseudo-classes like :visible, :has-text(), :has(), :is(), :nth-match() and more.
     add_note_button = page.locator('button:has-text("Add note")')
     expect(add_note_button).to_be_visible()
     add_note_button.click()
 
-    # T4.3 Klicka/markera textrutan "Click to change text"
+    # T5.2 Klicka/markera textrutan "Click to change text"
     note_text = page.locator('h3:has-text("Click to change text")').first
     expect(note_text).to_be_visible()
     note_text.click()
 
-    #[T4.4] Interagera/markera input fältet och skriv texten "Test T4.4"
+    #[T5.3] Interagera/markera input fältet och skriv texten "Test T5.3",
+    # verifiera att texten har ändrats till "TEST T5.3"
+    # Klicka med "Enter" för att spara och lämna textinmatningen
     edit_first_note_input = page.locator('input[placeholder="Description"]').first
     expect(edit_first_note_input).to_be_visible()
-    edit_first_note_input.fill("Test T4.4")
-
-    # [T4.5] Klicka med "Enter" för att spara och lämna textinmatningen
+    edit_first_note_input.fill("Test T5.3")
     page.keyboard.press('Enter')
-
-    # [T4.6] Kontrollera att texten har ändrats till "TEST T4.4"
-    edited_first_note_text = page.locator('h3:has-text("TEST T4.4")').first
+    edited_first_note_text = page.locator('h3:has-text("TEST T5.3")').first
     expect(edited_first_note_text).to_be_visible()
 
-    # [T4.7] enligt samma scenario från T4.2 - T4.6 och fyll i en ny ruta med texten "TEST T4.7"
+    # [T5.4] enligt samma scenario från T5.3 fyll i en ny ruta med texten "TEST T5.4"
     add_note_button.click() # klicka fram ny textinmatningsruta
     second_note_text = page.locator('h3:has-text("Click to change text")').last
     expect(second_note_text).to_be_visible() # vi checkar att det är en oskriven ruta
-
     second_note_text.click() # öppna för textinmatning
     edit_second_note_input = page.locator('input[placeholder="Description"]').last
-    edit_second_note_input.fill("TEST T4.7")
+    edit_second_note_input.fill("TEST T5.4")
     page.keyboard.press("Enter")
-    edited_second_note_text = page.locator('h3:has-text("TEST T4.7")').last
+    edited_second_note_text = page.locator('h3:has-text("TEST T5.4")').last
     expect(edited_second_note_text).to_be_visible()
 
-    #[T4.8] Testa att ändra ordningen på de två texterna samt kontrollera att det gjorts
+    #[T5.5] Testa att ändra ordningen på de två texterna samt kontrollera att det gjorts
     up_arrow_button = page.locator('.icon.up').first
     up_arrow_button.click()
-    expect(page.locator('h3:has-text("TEST T4.7")')).to_be_visible()
-    expect(page.locator('h3:has-text("TEST T4.4")')).to_be_visible()
+    expect(page.locator('h3:has-text("TEST T5.4")')).to_be_visible()
+    expect(page.locator('h3:has-text("TEST T5.3")')).to_be_visible()
 
-    #[T4.9] Testa att radera/stänga den första texten genom att klicka ikonen för papperskorg och kontrollera att det gjorts
+    #[T5.6] Testa att radera/stänga den första texten genom att klicka ikonen för papperskorg och kontrollera att det gjorts
     delete_button = page.locator('.icon.close').first
     delete_button.click()
-    expect(page.locator('h3:has-text("TEST T4.2")')).not_to_be_visible()
-    expect(page.locator('h3:has-text("TEST T4.7")')).to_be_visible()
+    expect(page.locator('h3:has-text("TEST T5.3")')).not_to_be_visible()
+    expect(page.locator('h3:has-text("TEST T5.4")')).to_be_visible()
